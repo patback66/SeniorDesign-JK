a4ca99cc23148ff9343c09722257d615be3b580d
Matt Koken <patback66@users.noreply.github.com>
2017-05-06 11:34:09 -0700

    update basic test for travis-ci

--- Project/electron-boilerplate-master/e2e/hello_world.e2e.js
+++ Project/electron-boilerplate-master/e2e/hello_world.e2e.js
@@ -6,9 +6,9 @@ describe('application launch', function () {
     beforeEach(testUtils.beforeEach);
     afterEach(testUtils.afterEach);
 
-    it('shows hello world text on screen after launch', function () {
-        return this.app.client.getText('#greet').then(function (text) {
-            expect(text).to.equal('Hello World!');
+    it('settings button after launch', function () {
+        return this.app.client.getText('#settings').then(function (text) {
+            expect(text).to.equal('Settings ⚙');
         });
     });
 });
