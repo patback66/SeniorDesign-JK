3866f019dbcacd12627f6e3fb98ea1aacbf8c0f7
Matthew Koken <maskoken@gmail.com>
2017-06-09 00:53:10 -0700

    cleanup, add api documentation

---
+++ Project/electron-boilerplate-master/src/out/api.js
@@ -0,0 +1,10 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "{Widget} Widget provides functionality for an html widget."
+    ],
+    "modules": [],
+    "allModules": [],
+    "elements": []
+} };
+});
